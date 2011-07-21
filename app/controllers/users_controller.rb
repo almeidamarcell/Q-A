ec08cf74b5e08ed6879d46c311a2e60e50a8f2b7
ITmeze <michal.zygula@gmail.com>
2011-07-22 01:14:09 +0300

    working on login partial view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,8 +26,26 @@ class UsersController < ApplicationController
     page = params[:page].nil? ? 1 : params[:page]
     size = params[:size].nil? ? 2 : params[:size]
 
-    @users = User.where('display_name LIKE ?', "%#{params[:search]}%").page(page).per(size)
-    render :partial => "/shared/allusers.html.erb"
+    order_column = :created_at
+
+    case params[:order]
+    when "newest"
+      order_column = :created_at
+    when "by_reputation"
+      order_column = :reputation
+    end
+
+    @order = params[:order].nil? ? "newest" : params[:order]
+
+    @users = User.where('display_name LIKE ?', "%#{params[:search]}%").order(order_column.to_s + " desc").page(page).per(size)
+
+    @page.h2 = 'All Users'
+
+    if request.xhr?
+      render :partial => "/shared/allusers.html.erb"
+    else
+      render "all"
+    end
   end
 
 end
